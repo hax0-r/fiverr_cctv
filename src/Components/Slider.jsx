7243153736757545004a360db34583b47506a70d
Muhammad <talha18513@gmail.com>
2024-10-14 21:38:48 +0500

    I added text to the file

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import img1 from '../assets/img1.png'
-import img2 from '../assets/img2.png'
+import img1 from '../assets/img1.png';
+import img2 from '../assets/img2.png';
+
+import { Mousewheel, Pagination } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
-
 
 const Slider = () => {
     return (
@@ -14,6 +15,8 @@ const Slider = () => {
                 slidesPerView={5}
                 spaceBetween={10}
                 className="mySwiper"
+                mousewheel={true}
+                modules={[Mousewheel, Pagination]}
                 breakpoints={{
                     320: {
                         slidesPerView: 1,
@@ -26,6 +29,7 @@ const Slider = () => {
                     },
                 }}
             >
+
 
                 <SwiperSlide>
                     <div className="">
@@ -104,50 +108,7 @@ const Slider = () => {
                         <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide>
-                    <div className="">
-                        <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
-                            <div className="p-1.5 flex items-center gap-2">
-                                <img src={img1} className="w-[70px] rounded-md h-[70px]" />
-                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#14B864] py-0.5 px-2 rounded-md'>1 min</span></h2>
-                            </div>
-                        </div>
-                        <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="">
-                        <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
-                            <div className="p-1.5 flex items-center gap-2">
-                                <img src={img1} className="w-[70px] rounded-md h-[70px]" />
-                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#14B864] py-0.5 px-2 rounded-md'>1 min</span></h2>
-                            </div>
-                        </div>
-                        <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="">
-                        <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
-                            <div className="p-1.5 flex items-center gap-2">
-                                <img src={img1} className="w-[70px] rounded-md h-[70px]" />
-                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#14B864] py-0.5 px-2 rounded-md'>1 min</span></h2>
-                            </div>
-                        </div>
-                        <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="">
-                        <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
-                            <div className="p-1.5 flex items-center gap-2">
-                                <img src={img1} className="w-[70px] rounded-md h-[70px]" />
-                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#14B864] py-0.5 px-2 rounded-md'>1 min</span></h2>
-                            </div>
-                        </div>
-                        <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
-                    </div>
-                </SwiperSlide>
+
             </Swiper>
         </>
     )
