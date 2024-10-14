f58392612e9c9a0025f799623cc77d00a7182164
Muhammad <talha18513@gmail.com>
2024-10-14 21:06:38 +0500

    I added text to the file

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -29,7 +29,18 @@ const Slider = () => {
                         <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
                             <div className="p-1.5 flex items-center gap-2">
                                 <img src="/public/img1.png" className="w-[70px] rounded-md h-[70px]" />
-                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#F5B920] py-0.5 px-2 rounded-md'>1 hour</span></h2>
+                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#14B864] py-0.5 px-2 rounded-md'>1 min</span></h2>
+                            </div>
+                        </div>
+                        <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="">
+                        <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
+                            <div className="p-1.5 flex items-center gap-2">
+                                <img src="/public/img2.png" className="w-[70px] rounded-md h-[70px]" />
+                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#14B864] py-0.5 px-2 rounded-md'>1 min</span></h2>
                             </div>
                         </div>
                         <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
@@ -40,7 +51,18 @@ const Slider = () => {
                         <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
                             <div className="p-1.5 flex items-center gap-2">
                                 <img src="/public/img1.png" className="w-[70px] rounded-md h-[70px]" />
-                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#F5B920] py-0.5 px-2 rounded-md'>1 hour</span></h2>
+                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#14B864] py-0.5 px-2 rounded-md'>1 min</span></h2>
+                            </div>
+                        </div>
+                        <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="">
+                        <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
+                            <div className="p-1.5 flex items-center gap-2">
+                                <img src="/public/img2.png" className="w-[70px] rounded-md h-[70px]" />
+                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#14B864] py-0.5 px-2 rounded-md'>1 min</span></h2>
                             </div>
                         </div>
                         <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
@@ -51,7 +73,7 @@ const Slider = () => {
                         <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
                             <div className="p-1.5 flex items-center gap-2">
                                 <img src="/public/img1.png" className="w-[70px] rounded-md h-[70px]" />
-                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#F5B920] py-0.5 px-2 rounded-md'>1 hour</span></h2>
+                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#14B864] py-0.5 px-2 rounded-md'>1 min</span></h2>
                             </div>
                         </div>
                         <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
@@ -62,7 +84,7 @@ const Slider = () => {
                         <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
                             <div className="p-1.5 flex items-center gap-2">
                                 <img src="/public/img1.png" className="w-[70px] rounded-md h-[70px]" />
-                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#F5B920] py-0.5 px-2 rounded-md'>1 hour</span></h2>
+                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#14B864] py-0.5 px-2 rounded-md'>1 min</span></h2>
                             </div>
                         </div>
                         <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
@@ -73,7 +95,7 @@ const Slider = () => {
                         <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
                             <div className="p-1.5 flex items-center gap-2">
                                 <img src="/public/img1.png" className="w-[70px] rounded-md h-[70px]" />
-                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#F5B920] py-0.5 px-2 rounded-md'>1 hour</span></h2>
+                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#14B864] py-0.5 px-2 rounded-md'>1 min</span></h2>
                             </div>
                         </div>
                         <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
@@ -84,7 +106,7 @@ const Slider = () => {
                         <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
                             <div className="p-1.5 flex items-center gap-2">
                                 <img src="/public/img1.png" className="w-[70px] rounded-md h-[70px]" />
-                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#F5B920] py-0.5 px-2 rounded-md'>1 hour</span></h2>
+                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#14B864] py-0.5 px-2 rounded-md'>1 min</span></h2>
                             </div>
                         </div>
                         <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
@@ -95,7 +117,7 @@ const Slider = () => {
                         <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
                             <div className="p-1.5 flex items-center gap-2">
                                 <img src="/public/img1.png" className="w-[70px] rounded-md h-[70px]" />
-                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#F5B920] py-0.5 px-2 rounded-md'>1 hour</span></h2>
+                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#14B864] py-0.5 px-2 rounded-md'>1 min</span></h2>
                             </div>
                         </div>
                         <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
@@ -106,29 +128,7 @@ const Slider = () => {
                         <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
                             <div className="p-1.5 flex items-center gap-2">
                                 <img src="/public/img1.png" className="w-[70px] rounded-md h-[70px]" />
-                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#F5B920] py-0.5 px-2 rounded-md'>1 hour</span></h2>
-                            </div>
-                        </div>
-                        <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="">
-                        <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
-                            <div className="p-1.5 flex items-center gap-2">
-                                <img src="/public/img1.png" className="w-[70px] rounded-md h-[70px]" />
-                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#F5B920] py-0.5 px-2 rounded-md'>1 hour</span></h2>
-                            </div>
-                        </div>
-                        <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="">
-                        <div className="bg-[#FFFFFF1A] w-full rounded-t-md border border-b-0 border-zinc-600">
-                            <div className="p-1.5 flex items-center gap-2">
-                                <img src="/public/img1.png" className="w-[70px] rounded-md h-[70px]" />
-                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#F5B920] py-0.5 px-2 rounded-md'>1 hour</span></h2>
+                                <h2 className='inline-flex flex-col text-white gap-0.5 justify-start items-start'>Waiting Time <span className='bg-[#14B864] py-0.5 px-2 rounded-md'>1 min</span></h2>
                             </div>
                         </div>
                         <p className='text-white p-1 bg-[#00000066] rounded-b-md'>Arrival time : 09.00PG</p>
